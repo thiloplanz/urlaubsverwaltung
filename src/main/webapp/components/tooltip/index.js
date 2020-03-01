b47d1b3098eb693982cde39a14ddfda17320ef12
Thilo Planz <thilo@cpan.org>
2020-03-01 12:59:48 +0800

    Fix tooltip visibility in department calendar

--- src/main/webapp/components/tooltip/index.js
+++ src/main/webapp/components/tooltip/index.js
@@ -1,6 +1,25 @@
 import $ from 'jquery';
 import 'bootstrap/js/tooltip';
 
+function yados_fix_tooltip_show() {
+  const td = $(this).closest('td')[0]
+  if (td){
+    td.style.overflow = "visible"
+  }
+}
+
+function yados_fix_tooltip_hide() {
+   const td = $(this).closest('td')[0]
+   if (td){
+     td.style.overflow = ""
+   }
+}
+
+
 export default function tooltip() {
-  $('[data-title]').attr('data-placement', 'bottom').tooltip();
+  $('[data-title]').attr('data-placement', 'bottom')
+    .tooltip()
+    .on('hidden.bs.tooltip', yados_fix_tooltip_hide)
+    .on('inserted.bs.tooltip', yados_fix_tooltip_show)
+
 }
